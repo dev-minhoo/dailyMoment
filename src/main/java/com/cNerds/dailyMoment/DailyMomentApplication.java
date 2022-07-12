7627a30b921a2b208c2cca316257411a545d7577
dev-minhoo <minhoo@naver.com>
2022-07-12 19:31:33 +0900

    gradle Update

---
+++ src/main/java/com/cNerds/dailyMoment/DailyMomentApplication.java
@@ -0,0 +1,13 @@
+package com.cNerds.dailyMoment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyMomentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyMomentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cNerds/dailyMoment/DailyMomentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cNerds.dailyMoment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyMomentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
